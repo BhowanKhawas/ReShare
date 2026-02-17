2a264bea3d301dff4c52d8f86d49bf8a7862a7f8
ismail-sohail25 <250453721+ismail-sohail25@users.noreply.github.com>
2026-02-17 01:06:37 +0000

    Lab 04: Node.js & Express routes, dynamic routes, static files completed

--- app/app.js
+++ app/app.js
@@ -1,72 +1,65 @@
-// Import express.js
-const express = require("express");
+// Require express
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Create express app
-var app = express();
+// Serve static files from 'static' folder
+app.use(express.static('static'));
 
-// Add static files location
-app.use(express.static("static"));
-
-// Get the functions in the db.js file to use
-const db = require('./services/db');
-
-// Create a route for root - /
+// Root route
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.send("Hello Ismail!");
 });
 
-// Create a route for testing the db
-app.get("/ITEMS", function(req, res) {
-I/ Assumes a table called test_table exists in your database
-var sql = 'select * from test_table';
-/ As we are not inside an async function we cannot use await / So we use .then syntax to ensure that we wait until the
-// promise returned by the async function is resolved before we proceed
-db.query (sql).then(results => {
-console.log(results);
-res.json(results)
-}) ;
-}) ;
-
-// Create a route for testing the db
-app.get("/USERS", function(req, res) {
-I/ Assumes a table called test_table exists in your database
-var sql = 'select * from test_table';
-/ As we are not inside an async function we cannot use await / So we use .then syntax to ensure that we wait until the
-// promise returned by the async function is resolved before we proceed
-db.query (sql).then(results => {
-console.log(results);
-res.json(results)
-}) ;
-}) ;
-
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+// Simple route
+app.get("/roehampton", function(req, res) {
+    console.log(req.url);
+    let path = req.url;
+    res.send(path.substring(0,3)); // first 3 letters
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
+// Dynamic route for /hello/:name
 app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
     console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
     res.send("Hello " + req.params.name);
 });
 
-// Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+// Dynamic route for /user/:id
+app.get("/user/:id", function(req, res) {
+    res.send("User ID: " + req.params.id);
+});
+
+// Dynamic route for /student/:name/:id
+app.get("/student/:name/:id", function(req, res) {
+    let name = req.params.name;
+    let id = req.params.id;
+    res.send(`<table border="1">
+                <tr><th>Name</th><th>ID</th></tr>
+                <tr><td>${name}</td><td>${id}</td></tr>
+              </table>`);
+});
+
+// Dynamic route for /number/:n
+app.get("/number/:n", function(req, res) {
+    let n = parseInt(req.params.n);
+    let html = "<table border='1'><tr>";
+    for(let i=0; i<=n; i++) {
+        html += "<td>" + i + "</td>";
+    }
+    html += "</tr></table>";
+    res.send(html);
+});
+
+// /roehampton route with reversed string logic
+app.get("/reverse_roehampton", function(req, res) {
+    let path = "/roehampton";
+    let arr = path.split(''); // split string to array
+    arr.splice(0,1); // remove leading '/'
+    let reversed = arr.reverse().join('');
+    res.send(reversed);
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
