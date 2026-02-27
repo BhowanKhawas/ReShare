8350f24f19015b7028278a6a29d5935756fa4e5e
BhowanKhawas <khawasb@roehampton.ac.uk>
2026-02-27 19:23:26 +0000

    login page code

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ const db = require('./services/db');
 
 // Create a route for root
 app.get("/", function(req, res) {
-    res.render("index");
+    res.render("login");
 });
 
 // Create a route for testing the db
